bd43e0d965870dc05ecc1040308093d0bc042c9b
BlueMoto <126126111+BlueMoto@users.noreply.github.com>
2026-07-12 15:49:38 +0800

    Update 99-custom.sh
     # 默认第二个接口为WAN，其余为LAN
            wan_ifname=$(echo "$ifnames" | awk '{print $2}')

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -51,8 +51,8 @@ case "$board_name" in
         echo "Using $board_name mapping: WAN=$wan_ifname LAN=$lan_ifnames" >>"$LOGFILE"
         ;;
     *)
-        # 默认第一个接口为WAN，其余为LAN
-        wan_ifname=$(echo "$ifnames" | awk '{print $1}')
+        # 默认第二个接口为WAN，其余为LAN
+        wan_ifname=$(echo "$ifnames" | awk '{print $2}')
         lan_ifnames=$(echo "$ifnames" | cut -d ' ' -f2-)
         echo "Using default mapping: WAN=$wan_ifname LAN=$lan_ifnames" >>"$LOGFILE"
         ;;
